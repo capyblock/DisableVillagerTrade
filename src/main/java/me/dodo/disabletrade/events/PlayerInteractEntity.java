e9900319a177444673b00b8a0643f87df41d0df0
dodoflix <dothish4nz@gmail.com>
2021-10-09 02:45:15 +0300

    Version 1.2

--- src/main/java/me/dodo/disabletrade/events/PlayerInteractEntity.java
+++ src/main/java/me/dodo/disabletrade/events/PlayerInteractEntity.java
@@ -8,26 +8,35 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class PlayerInteractEntity implements Listener {
     private static DisableTrade plugin;
     private static String message;
+    private static List<String> disabledWorlds = new ArrayList<>();
 
     public PlayerInteractEntity(DisableTrade _plugin) {
         plugin = _plugin;
-        message = ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(plugin.getConfig().getString("message")));
+        if (plugin.getConfig().getBoolean("message.enabled"))
+            message = ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(plugin.getConfig().getString("message.context")));
+        disabledWorlds = (List<String>) _plugin.getConfig().getList("disabled-worlds");
     }
 
     @EventHandler
     public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
         if (event.getRightClicked().getType() != EntityType.VILLAGER)
             return;
+        if (disabledWorlds.contains(event.getPlayer().getWorld().getName()))
+            return;
         if (event.getPlayer().hasPermission("disabletrade.bypass"))
             return;
+
         Villager villager = (Villager) event.getRightClicked();
         if (villager.hasAI()) {
-            event.getPlayer().sendMessage(message);
+            if (plugin.getConfig().getBoolean("message.enabled"))
+                event.getPlayer().sendMessage(message);
             event.setCancelled(true);
         }
     }
